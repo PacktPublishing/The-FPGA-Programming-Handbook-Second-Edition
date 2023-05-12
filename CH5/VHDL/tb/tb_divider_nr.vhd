4958db4f55b76de0bde239280f413a55de3884fe
Frank Bruno <fbruno@asicsolutions.com>
2023-05-12 13:05:40 -0500

    changed divider_nr to use VHD testbench

--- CH5/VHDL/tb/tb_divider_nr.vhd
+++ CH5/VHDL/tb/tb_divider_nr.vhd
@@ -1,74 +1,106 @@
-`timescale 1ns/10ps
-module tb;
-  parameter BITS = 16;
-  bit             clk;
-  bit             start;
-  bit [BITS-1:0]  dividend;
-  bit [BITS-1:0]  divisor;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.math_real.all;
+use std.env.finish;
+entity tb is
+end entity tb;
+architecture tb of tb is
+  procedure wait_nclk(signal clk: std_ulogic; n: positive) is
+  begin
+    for i in 1 to n loop
+      wait until rising_edge(clk);
+    end loop;
+  end procedure wait_nclk;
+  constant BITS : integer := 16;
+  signal clk       : std_logic := '0';
+  signal start     : std_logic;
+  signal dividend  : std_logic_vector(BITS-1 downto 0);
+  signal divisor   : std_logic_vector(BITS-1 downto 0);
+  signal done      : std_logic;
+  signal quotient  : std_logic_vector(BITS-1 downto 0);
+  signal remainder : std_logic_vector(BITS-1 downto 0);
+begin
 
-  bit             done;
-  bit unsigned [BITS-1:0] quotient;
-  bit unsigned [BITS-1:0] remainder;
+  clock : process
+  begin
+    while true loop
+      clk <= not clk;
+      wait for 5 ns;
+    end loop;
+  end process clock;
 
-  initial clk = '0;
-  always begin
-    clk = #5 ~clk;
-  end
+  u_divider_nr : entity work.divider_nr
+    generic map(BITS =>BITS)
+    port map(clk       => clk,
+             start     => start,
+             dividend  => dividend,
+             divisor   => divisor,
+             done      => done,
+             quotient  => quotient,
+             remainder => remainder);
 
-  divider_nr
-  #
-  (
-   .BITS (BITS)
-   )
-  u_divider_nr
-  (
-   .*
-   );
+  stim : process 
+    variable seed1, seed2: positive;  -- seed values for random number generator
+    variable rand_val: real;              -- random real value 0 to 1.0
+  begin
+    start    <= '0';
+    dividend <= (others => '0');
+    divisor  <= (others => '0');
+    wait_nclk(clk, 5); -- equivalent to repeat (5) @(posedge clk); in SV
 
-  initial begin
-    start    = '0;
-    dividend = '0;
-    divisor  = '0;
-    repeat (5) @(posedge clk);
-    dividend <= 16'd11;
-    divisor  <= 16'd3;
-    start    <= '1;
-    @(posedge clk);
-    start    <= '0;
-    while (!done) @(posedge clk);
-    repeat (5) @(posedge clk);
-    for (int i = 0; i < 100; i++) begin
-      dividend <= $random;
-      divisor  <= $random;
-      start    <= '1;
-      @(posedge clk);
-      start    <= '0;
-      while (!done) @(posedge clk);
-      repeat (5) @(posedge clk);
-    end
-    // test divide by 0
-    dividend = '0;
-    divisor  = '0;
-    start    <= '1;
-    @(posedge clk);
-    start    <= '0;
-    while (!done) @(posedge clk);
-    repeat (5) @(posedge clk);
-
-    repeat (5) @(posedge clk);
-    $finish;
-  end // initial begin
-
-  always @(posedge clk) begin
-    if (done &&
-        (quotient != dividend/divisor) &&
-        (remainder != dividend%divisor)) begin
-      $display("failure!");
-      $display("quotient:   %d", quotient);
-      $display("remainder:  %d", remainder);
-      $display("expected Q: %d", dividend/divisor);
-      $display("expected R: %d", dividend%divisor);
-      $stop;
-    end
-  end
-endmodule // tb
+    dividend <= 0x"000b";
+    divisor  <= 0x"0003";
+    start    <= '1';
+    wait until rising_edge(clk);
+    start    <= '0';
+    while not(done) loop
+      wait until rising_edge(clk);
+    end loop;
+    wait_nclk(clk, 5); -- equivalent to repeat (5) @(posedge clk); in SV
+    for i in 0 to 99 loop
+      uniform(seed1, seed2, rand_val);              -- generate random number
+      dividend <= std_logic_vector(to_unsigned(integer(trunc(rand_val*65536.0)), dividend'length));
+      uniform(seed1, seed2, rand_val);              -- generate random number
+      divisor  <= std_logic_vector(to_unsigned(integer(trunc(rand_val*65536.0)), dividend'length));
+      start    <= '1';
+      wait until rising_edge(clk);
+      start    <= '0';
+      while not(done) loop
+        wait until rising_edge(clk);
+      end loop;
+      wait_nclk(clk, 5); -- equivalent to repeat (5) @(posedge clk); in SV
+    end loop;
+    -- test divide by 0
+    -- VHDL isn't as forgiving as SV so skipping this test
+    --dividend <= (others => '0');
+    --divisor  <= (others => '0');
+    --start    <= '1';
+    --wait until rising_edge(clk);
+    --start    <= '0';
+    --while not(done) loop
+    --  wait until rising_edge(clk);
+    --end loop;
+    wait_nclk(clk, 10); -- equivalent to repeat (10) @(posedge clk); in SV
+    finish;
+  end process stim;
+  
+  check : process (clk) 
+    variable divd : integer;
+    variable divi : integer;
+  begin
+    divd := to_integer(unsigned(dividend));
+    divi := to_integer(unsigned(divisor));
+    if rising_edge(clk) then
+      if done = '1' and 
+        (quotient /= std_logic_vector(to_unsigned(divd/divi, quotient'length))) and
+        (remainder /= std_logic_vector(to_unsigned(divd mod divi, remainder'length))) then
+        report "FAILURE!";
+        report "quotient:   " & to_string(quotient);
+        report "remainder:  " & to_string(remainder);
+        report "Expected Q: " & to_string(divd/divi);
+        report "Expected R: " & to_string(divd mod divi);
+      end if;
+    end if;
+  end process check;
+end architecture tb;
