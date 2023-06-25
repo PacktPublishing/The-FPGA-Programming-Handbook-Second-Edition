af192d3ec327c14f5f3b2c60e1471a08461bb419
Guy Eschemann <Guy.Eschemann@plc2.de>
2023-06-25 10:14:22 +0200

    cleanup CH3 VHDL

--- CH3/VHDL/hdl/num_ones.vhd
+++ CH3/VHDL/hdl/num_ones.vhd
@@ -4,27 +4,35 @@
 -- ------------------------------------
 -- Author : Frank Bruno
 -- Count the number of bits that are high in a vector
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.math_real.all;
 
 entity num_ones is
-  generic(BITS : integer := 16);
-  port(SW: in std_logic_vector(BITS-1 downto 0);
-       LED: out std_logic_vector(natural(log2(real(BITS))) downto 0));
+    generic(
+        BITS : integer := 16
+    );
+    port(
+        SW  : in  std_logic_vector(BITS - 1 downto 0);
+        LED : out std_logic_vector(natural(ceil(log2(real(BITS)))) downto 0)
+    );
 end entity num_ones;
 
 architecture rtl of num_ones is
 begin
 
-  counter : process (all)
-    variable count : unsigned(natural(log2(real(BITS))) downto 0);
-  begin
-    count := (others => '0');
-    for i in SW'range loop
-      count := count + SW(i);
-    end loop;
-    LED <= std_logic_vector(count);
-  end process counter;
+    counter : process(all)
+        variable count : natural range 0 to BITS;
+    begin
+        count := 0;
+        for i in SW'range loop
+            if SW(i) then
+                count := count + 1;
+            end if;
+        end loop;
+        LED   <= std_logic_vector(to_unsigned(count, LED'length));
+    end process counter;
+
 end architecture rtl;
