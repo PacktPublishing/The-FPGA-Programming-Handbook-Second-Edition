380a8036b66cafbdde13eb26dffd74c99cda7fc7
Guy Eschemann <guy@noasic.com>
2023-07-08 14:32:41 +0200

    cleanup

--- CH5/VHDL/hdl/calculator_moore.vhd
+++ CH5/VHDL/hdl/calculator_moore.vhd
@@ -29,11 +29,11 @@ architecture rtl of calculator_moore is
 
   constant BC : natural := natural(ceil(log2(real(BITS))));
 
-  type state_t is (IDLE, WAIT4BUTTON, ADD, SUB, MULT);
+  type state_t is (IDLE, WAIT4BUTTON, ADD, SUB, MULT, RESET_S);
 
   signal op_store, last_op : std_logic_vector(4 downto 0);
-  signal accumulator       : std_logic_vector(BITS - 1 downto 0) := (others => '0');
-  signal state             : state_t                             := IDLE;
+  signal accumulator       : unsigned(BITS - 1 downto 0) := (others => '0');
+  signal state             : state_t                     := IDLE;
 
   attribute MARK_DEBUG : string;
   attribute MARK_DEBUG of op_store : signal is "TRUE";
@@ -44,9 +44,7 @@ architecture rtl of calculator_moore is
 begin
 
   process(clk)
-    variable multiply   : unsigned(BITS + 16 - 1 downto 0);
-    variable switch_int : unsigned(15 downto 0);
-    variable accum_int  : unsigned(31 downto 0);
+    variable product : unsigned(BITS + 16 - 1 downto 0);
   begin
     if rising_edge(clk) then
       if reset then
@@ -61,15 +59,16 @@ begin
             if start then
               state <= WAIT4BUTTON;
             end if;
+
           when WAIT4BUTTON =>
-            -- wait for second data to be entered, then user presses next operation.
+            -- Wait for second data to be entered, then user presses next operation.
             -- In this case, if we get an =, we perform the operation and we're
             -- done. The user can also put in another operation to perform with
             -- a new value on the accumulator.
             if last_op(UP) then
               state <= MULT;
             elsif last_op(DOWN) then
-              state <= IDLE;
+              state <= RESET_S;
             elsif last_op(LEFT) then
               state <= ADD;
             elsif last_op(RIGHT) then
@@ -77,27 +76,29 @@ begin
             else
               state <= IDLE;
             end if;
+
           when MULT =>
-            switch_int  := unsigned(switch);
-            multiply    := unsigned(accumulator) * switch_int;
-            -- note that even though the output is > 32 bits we will overflow
+            product     := accumulator * unsigned(switch);
+            -- Note that even though the output is > 32 bits we will overflow
             -- if larger.
-            accumulator <= std_logic_vector(multiply(31 downto 0));
+            accumulator <= product(accumulator'range);
             state       <= IDLE;
+
           when ADD =>
-            switch_int  := unsigned(switch);
-            accum_int   := unsigned(accumulator) + switch_int;
-            accumulator <= std_logic_vector(accum_int(31 downto 0));
+            accumulator <= accumulator + unsigned(switch);
             state       <= IDLE;
+            
           when SUB =>
-            switch_int  := unsigned(switch);
-            accum_int   := unsigned(accumulator) - switch_int;
-            accumulator <= std_logic_vector(accum_int(31 downto 0));
+            accumulator <= accumulator - unsigned(switch);
+            state       <= IDLE;
+
+          when RESET_S =>
+            accumulator <= (others => '0');
             state       <= IDLE;
         end case;
       end if;
     end if;
   end process;
 
-  accum <= accumulator;
+  accum <= std_logic_vector(accumulator);
 end;
