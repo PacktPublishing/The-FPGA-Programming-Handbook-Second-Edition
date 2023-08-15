3852ecb2708c4143f661756feaed8364d8131c8f
Guy Eschemann <guy@noasic.com>
2023-08-15 19:43:54 +0200

    disable incremental synthesis

--- CH8/VHDL/build/completed/i2c_temp_flt_bd.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ CH8/VHDL/build/completed/i2c_temp_flt_bd.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Sun Aug 13 19:06:56 2023
+--Date        : Tue Aug 15 18:18:38 2023
 --Host        : DESKTOP-3TFI5BO running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
